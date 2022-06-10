404be1cf72e907b4292848895f0ebbce922e70e3
majate <majate@kth.se>
2022-06-11 00:50:06 +0200

    Add material UI

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+  <section>
+    {props.name} is mounted!
+    <Button>Hello!</Button>
+  </section>
+  );
 }
